ce77978ac3efd83bb22499e87e356815e058e9ba
Joey Bright <hello@joeybright.com>
2024-03-29 15:37:06 -0700

    Added browser vs. node detection for crypto

--- src/Gren/Kernel/Crypto.js
+++ src/Gren/Kernel/Crypto.js
@@ -4,11 +4,16 @@ import Gren.Kernel.Scheduler exposing (binding, succeed)
 
 */
 
+var crypto = function () {
+    if (typeof window === 'undefined') {
+        return require('crypto');
+    }
+    return window.crypto;
+}
+
 var _Crypto_randomUUID = function (_) {
     return __Scheduler_binding(function (callback) {
-        var randomUUID;
-        const crypto = require('crypto');
-        randomUUID = crypto.randomUUID();
+        var randomUUID = crypto().randomUUID();
         return callback(__Scheduler_succeed(randomUUID));
     });
 }
