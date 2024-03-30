167546f484d6855208dacf6337952e04191f50e4
Joey Bright <hello@joeybright.com>
2024-03-29 17:47:28 -0700

    Call the `crypto` function after its defined

--- src/Gren/Kernel/Crypto.js
+++ src/Gren/Kernel/Crypto.js
@@ -10,24 +10,24 @@ var crypto = function () {
         return require('crypto');
     }
     return window.crypto;
-};
+}();
 
 var _Crypto_randomUUID = __Scheduler_binding(function (callback) {
-    var randomUUID = crypto().randomUUID();
+    var randomUUID = crypto.randomUUID();
     return callback(__Scheduler_succeed(randomUUID));
 });
 
 var _Crypto_getRandomValues = function (arrayLength) {
     return __Scheduler_binding(function (callback) {
         var array = new Int8Array(arrayLength);
-        var randomValues = crypto().getRandomValues(array);
+        var randomValues = crypto.getRandomValues(array);
         return callback(__Scheduler_succeed(randomValues));
     })
 };
 
 var _Crypto_getContext = __Scheduler_binding(function (callback) {
     var context;
-    if (crypto().subtle) {
+    if (crypto.subtle) {
         context = __Crypto_SecureContext
     } else {
         context = __Crypto_InsecureContext
