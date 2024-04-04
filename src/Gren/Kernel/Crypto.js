06d82fa2b7a7fecfd8b91af3458e207d842fff00
Joey Bright <hello@joeybright.com>
2024-04-03 22:16:47 -0700

    Added support for generating keys, encrypting, and decrypting with RSA-OAEP algorithm

--- src/Gren/Kernel/Crypto.js
+++ src/Gren/Kernel/Crypto.js
@@ -1,7 +1,10 @@
 /*
 
-import Gren.Kernel.Scheduler exposing (binding, succeed)
-import Crypto exposing (SecureContext, InsecureContext)
+import Gren.Kernel.Scheduler exposing (binding, succeed, fail)
+import Gren.Kernel.Bytes exposing (writeBytes)
+import Crypto exposing (KeyPair, Key, SecureContext, InsecureContext)
+import Maybe exposing (Just, Nothing)
+import Bytes exposing (Bytes)
 
 */
 
@@ -26,11 +29,58 @@ var _Crypto_getRandomValues = function (arrayLength) {
 };
 
 var _Crypto_getContext = __Scheduler_binding(function (callback) {
-    var context;
     if (crypto.subtle) {
-        context = __Crypto_SecureContext
-    } else {
-        context = __Crypto_InsecureContext
+        return callback(__Scheduler_succeed(__Crypto_SecureContext));
     }
-    return callback(__Scheduler_succeed(context));
+    return callback(__Scheduler_fail);
+});
+
+var _Crypto_generateKey = F3(function (algorithm, extractable, permissions) {
+    if (algorithm.__$publicExponent) {
+        // Creating a new Uint8Array if the passed algorithm contains a publicExponent
+        // Unsure how to pass this value directly from Gren and this is the current workaround
+        algorithm.__$publicExponent = new Uint8Array(algorithm.__$publicExponent);
+    }
+    return __Scheduler_binding(function (callback) {
+        crypto.subtle
+            .generateKey(algorithm, extractable, permissions)
+            .then(function (key) {
+                if (key.publicKey && key.privateKey) {
+                    return callback(__Scheduler_succeed(
+                        __Crypto_KeyPair(
+                            {
+                                publicKey: __Crypto_Key(key.__$publicKey),
+                                privateKey: __Crypto_Key(key.__$privateKey)
+                            }
+                        )
+                    ));
+                } else {
+                    return callback(__Scheduler_succeed(__Crypto_Key(key)));
+                };
+            });
+    });
+});
+
+var _Crypto_encrypt = F3(function (key, params, data) {
+    return __Scheduler_binding(function (callback) {
+        crypto.subtle.encrypt(params, key.__$a, data)
+            .then(function (res) {
+                return callback(__Scheduler_succeed(new DataView(res)));
+            })
+            .catch(function (err) {
+                return callback(__Scheduler_fail(err));
+            });
+    });
+});
+
+var _Crypto_decrypt = F3(function (key, params, data) {
+    return __Scheduler_binding(function (callback) {
+        crypto.subtle.decrypt(params, key.__$a, data)
+            .then(function (res) {
+                return callback(__Scheduler_succeed(new DataView(res)));
+            })
+            .catch(function (err) {
+                return callback(__Scheduler_fail(err));
+            })
+    });
 });
