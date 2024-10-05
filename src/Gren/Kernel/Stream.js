1af476db041a22f4689299dd24902e9f3e0b6e19
Robin Heggelund Hansen <git@heghan.org>
2024-10-05 23:01:29 +0200

    Remove conversion to Uint8Array, streams should treat values as opaque. Also make sure to release lock on reader/writer when done.

--- src/Gren/Kernel/Stream.js
+++ src/Gren/Kernel/Stream.js
@@ -7,26 +7,37 @@ import Gren.Kernel.Scheduler exposing (binding, succeed, fail, rawSpawn)
 
 var _Stream_read = function(stream) {
   return __Scheduler_binding(function(callback) {
-    stream.getReader()
+    const reader = stream.getReader();
+    reader
       .read()
-      .then(({ value }) => callback(__Scheduler_succeed(value)))
+      .then(({ value }) => {
+        callback(__Scheduler_succeed(value))
+      })
       .catch((err) => {
-          console.log("Stream err: ", err);
-          return callback(__Scheduler_fail(__Stream_Closed));
-        }
-      )
+        console.log("ReadableStream err: ", err);
+        return callback(__Scheduler_fail(__Stream_Closed));
+      })
+      .finally(() => {
+        reader.releaseLock();
+      })
   });
 };
 
 var _Stream_write = F2(function(value, stream) {
   return __Scheduler_binding(function(callback) {
-    stream.getWriter()
-      .write(new Uint8Array(value.buffer, value.byteOffset, value.byteLength))
-      .then(() => callback(__Scheduler_succeed({})))
+    const writer = stream.getWriter();
+    writer
+      .write(value)
+      .then(() => {
+        callback(__Scheduler_succeed({}))
+      })
       .catch((err) => {
-          console.log("Stream err: ", err);
+          console.log("WriteableStream err: ", err);
           return callback(__Scheduler_fail(__Stream_Closed));
         }
       )
+      .finally(() => {
+        writer.releaseLock();
+      })
   });
 });
