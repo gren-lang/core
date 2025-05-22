cf8590dd85743ffd2730742e4b275eefe1be84f4
Robin Heggelund Hansen <git@heghan.org>
2025-05-22 14:36:17 +0200

    Undo silly change.

--- src/Gren/Kernel/Stream.js
+++ src/Gren/Kernel/Stream.js
@@ -4,6 +4,7 @@ import Stream exposing (Locked, Closed, Cancelled)
 import Gren.Kernel.Scheduler exposing (binding, succeed, fail, rawSpawn)
 
 */
+
 var _Stream_read = function (stream) {
   return __Scheduler_binding(function (callback) {
     if (stream.locked) {
